1e6630427b9949431174fe4f2b21a1942e3dc983
Zach Lariccia <sadboigoon@gmail.com>
2025-06-05 13:52:43 -0400

    Incorporated Two-Dimensional Array for computational efficiency and further exploration

--- src/RunningLog.java
+++ src/RunningLog.java
@@ -5,54 +5,76 @@ public class RunningLog {
         Scanner sc = new Scanner(System.in); // Initiate Scanner
         System.out.println("Enter number of runs that you went on: "); //Prompts user to enter runs
         int numberOfRuns = sc.nextInt(); // Captures number of runs
-        double distance;
         double totalDistance = 0;
-        double time;
         double totalTime = 0;
         double averagePace;
+        double[][] runs = new double[numberOfRuns][2]; // Distance is runs[i][0], time is runs[i][1] respectively
+        int runCounter = 1; // Introduced a run counter for tracking
 
-        for (int i = 0; i < numberOfRuns; ++i) {/*
-        /*
-        O(n) time complexity of a 'for loop'
-        which loops through and asks user for each run,
-        what the distance was and the time it took
-        */
+        for (int i = 0; i < numberOfRuns; ++i) {
             System.out.println("How long was the distance in miles? (Use Decimal Format): ");
-            distance = sc.nextDouble();
-            if (distance <= 0){ // Conditional statement to handle the <=0 case
+            runs[i][0] = sc.nextDouble(); // Asks the user for distance and sets it in the matrix
+            if (runs[i][0] <= 0) { // Conditional statement to handle the <=0 case
                 System.out.println("Please enter a valid value, Please try again");
                 i--;
                 continue;
             }
-            totalDistance += distance; // Adding next distance value to total distance
+            totalDistance += runs[i][0]; // Adding next distance value to total distance
             System.out.println("What was the time in minutes? (Use Decimal Format): ");
-            time = sc.nextDouble();
-            if (time <= 0){ // Conditional statement to handle the <=0 case
+            runs[i][1] = sc.nextDouble();
+            if (runs[i][1] <= 0) { // Conditional statement to handle the <=0 case
                 System.out.println("Please enter a value higher than 0, Please try again");
                 i--;
+                totalDistance -= runs[i][0];
                 continue;
             }
-            totalTime += time; // Adding next time value to total time
-
-
+            totalTime += runs[i][1]; // Adding next time value to total time
+            double eachPace = runs[i][1] / runs[i][0];
+            System.out.println("Run " + runCounter + " you had an average pace of " + eachPace + " minutes.");
+            System.out.println();
+            runCounter++;
         }
         averagePace = totalTime / totalDistance; // Computing average pace
-        System.out.println("You're Average Pace Was " + averagePace + " Minutes"); // Did not format to 2 decimal places for better accuracy
+        System.out.println("Your Overall Average Pace Was " + averagePace + " Minutes"); // Did not format to 2 decimal places for better accuracy
+        System.out.println();
+
+        // Displaying table for readability alongside values
+        int runCounterForDisplay = 1;
+        System.out.println("-------------------Running Chart--------------------");
+        for (int i = 0; i < runs.length; i++) {
+            System.out.print("|" + "Run " + runCounterForDisplay + ":");
+            System.out.print(" Distance = " + runs[i][0] + " miles" + " | ");
+            System.out.print(" Time = " + runs[i][1] + " minutes" + "|");
+            System.out.println();
+            System.out.println("----------------------------------------------------");
+            runCounterForDisplay++;
+        }
 
         sc.close();
     }
 }
 
-/* Example Output:
+/* OUTPUT
 Enter number of runs that you went on:
 2
 How long was the distance in miles? (Use Decimal Format):
-3.1
+3.2
 What was the time in minutes? (Use Decimal Format):
-23.9
+25
+Run 1 you had an average pace of 7.8125 minutes.
+
 How long was the distance in miles? (Use Decimal Format):
-3.5
+2
 What was the time in minutes? (Use Decimal Format):
-24.2
-You're Average Pace Was 7.287878787878787 Minutes
+18
+Run 2 you had an average pace of 9.0 minutes.
+
+Your Overall Average Pace Was 8.269230769230768 Minutes
+
+-------------------Running Chart--------------------
+|Run 1: Distance = 3.2 miles |  Time = 25.0 minutes|
+----------------------------------------------------
+|Run 2: Distance = 2.0 miles |  Time = 18.0 minutes|
+----------------------------------------------------
+
  */
